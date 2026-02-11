69f3c8c4df8154c1880726a84ef5de668b1a8e82
Obinna Anosike <smartlify09@gmail.com>
2026-02-11 10:26:59 +0100

    build sidebar

---
+++ components/nav-main.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+export function NavMain({
+  items,
+}: {
+  items: {
+    title: string;
+    url: string;
+    icon?: React.ReactNode;
+  }[];
+}) {
+  const pathname = usePathname();
+  return (
+    <SidebarGroup>
+      <SidebarGroupContent className="flex flex-col gap-2">
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <Link href={item.url} passHref>
+                <SidebarMenuButton
+                  className={cn(
+                    pathname === item.url
+                      ? 'bg-sidebar-accent text-sidebar-accent-foreground'
+                      : '',
+                  )}
+                  tooltip={item.title}
+                >
+                  {item.icon}
+                  <span>{item.title}</span>
+                </SidebarMenuButton>
+              </Link>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+}
