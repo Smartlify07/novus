a89d45a2dfeb72788e3797430641b9b15a44bb73
Obinna Anosike <smartlify09@gmail.com>
2026-03-16 21:27:57 +0100

    feat: build header

---
+++ app/(app)/(dashboard)/loans/apply/page.tsx
@@ -0,0 +1,10 @@
+import Header from '@/app/features/loans/loan-application/components/header';
+import React from 'react';
+
+export default function LoanApplicationPage() {
+  return (
+    <div className="py-10 px-6">
+      <Header />
+    </div>
+  );
+}
