741cae480fdca5f629a9cc3a7a415f72b1a74f29
Obinna Anosike <smartlify09@gmail.com>
2026-03-19 16:22:59 +0100

    feat(loan-application): show success screen after submission

--- store/loan-application-workflow-store.ts
+++ store/loan-application-workflow-store.ts
@@ -9,23 +9,27 @@ type LoanApplicationWorkflowStore = Nullable<LoanApplicationPayload> & {
   termMonths: number;
   purpose: string;
   principalAmount: number;
+  isSubmitted: boolean;
   setLoanType: (loanType: LoanType) => void;
   setTermMonths: (months: number) => void;
   setStep: (step: LoanApplicationWorkflowStore['step']) => void;
   setPrincipalAmount: (principalAmount: number) => void;
   setPurpose: (purpose: string) => void;
+  setIsSubmitted: (isSubmitted: boolean) => void;
 };
 export const useLoanApplicationWorkflowStore =
   create<LoanApplicationWorkflowStore>((set, get) => ({
     accountId: null,
     loanType: null,
-    termMonths: 12, // default value of 12
+    termMonths: 12,
     purpose: '',
     principalAmount: 50000,
     step: 'type',
+    isSubmitted: false,
     setLoanType: (loanType) => set({ loanType }),
     setTermMonths: (months) => set({ termMonths: months }),
     setStep: (step) => set({ step }),
     setPrincipalAmount: (principalAmount) => set({ principalAmount }),
     setPurpose: (purpose) => set({ purpose }),
+    setIsSubmitted: (isSubmitted) => set({ isSubmitted }),
   }));
