5647a864a2e90f84a8f5c0c153f9b33c25304d33
Obinna Anosike <smartlify09@gmail.com>
2026-03-19 15:02:07 +0100

    feat(loan-application): create review step component

---
+++ app/features/loans/loan-application/components/review-step.tsx
@@ -0,0 +1,94 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardTitle,
+} from '@/components/ui/card';
+import { useStepper } from './stepper';
+import { useLoanApplicationWorkflowStore } from '@/store/loan-application-workflow-store';
+import { calculateInterest, calculateMonthlyPayment } from '@/lib/loan-utils';
+import { formatCurrency } from '@/lib/utils';
+
+export default function ReviewStep() {
+  const stepper = useStepper();
+  const { loanType, principalAmount, termMonths } =
+    useLoanApplicationWorkflowStore();
+  console.log(loanType, principalAmount, termMonths);
+  const interestRate = 3.4;
+  const monthlyPayment = calculateMonthlyPayment(
+    principalAmount,
+    interestRate,
+    termMonths,
+  );
+
+  const formatLoanType = (type: string | null) => {
+    if (!type) return '';
+    return type.charAt(0) + type.slice(1).toLowerCase();
+  };
+
+  return (
+    <Card className="p-6 max-w-xl flex flex-col gap-6">
+      <div className="flex flex-col gap-2">
+        <CardTitle className="text-2xl tracking-tighter">
+          Review your application
+        </CardTitle>
+        <CardDescription className="tracking-tight">
+          Confirm all details before submitting
+        </CardDescription>
+      </div>
+
+      <Card className="p-6 flex flex-col gap-4 bg-primary/5">
+        <div className="flex flex-col gap-2">
+          <CardDescription className="text-sm uppercase tracking-tight">
+            {formatLoanType(loanType)} Loan
+          </CardDescription>
+          <CardTitle className="text-5xl tracking-tighter">
+            {formatCurrency(principalAmount, 'NGN')}
+          </CardTitle>
+        </div>
+
+        <CardFooter className="bg-transparent  border-t-0 rounded-none px-0 pt-0 grid grid-cols-3 gap-4">
+          <div className="flex flex-col gap-1">
+            <p className="text-xs text-muted-foreground tracking-tight">Term</p>
+            <p className="text-base font-medium tracking-tight">
+              {termMonths} months
+            </p>
+          </div>
+          <div className="flex flex-col gap-1">
+            <p className="text-xs text-muted-foreground">Monthly</p>
+            <p className="text-base font-medium tracking-tight">
+              {formatCurrency(monthlyPayment, 'NGN')}
+            </p>
+          </div>
+          <div className="flex flex-col gap-1">
+            <p className="text-xs text-muted-foreground">Rate</p>
+            <p className="text-base font-medium tracking-tight">
+              {interestRate}%
+            </p>
+          </div>
+        </CardFooter>
+      </Card>
+
+      <CardFooter className="bg-card rounded-none px-0 flex items-center justify-between">
+        <p className="text-muted-foreground text-sm">
+          Step <span className="text-foreground">5</span> of 5
+        </p>
+
+        <div className="flex items-center gap-2">
+          <Button
+            variant={'outline'}
+            onClick={() => {
+              stepper.onChange({ id: 4, value: 'purpose' });
+            }}
+          >
+            Back
+          </Button>
+          <Button variant={'default'}>Continue</Button>
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
--- lib/loan-utils.ts
+++ lib/loan-utils.ts
@@ -1,8 +1,11 @@
 export function calculateMonthlyPayment(
   principal: number,
   rate: number,
-  numberOfMonths: number,
+  numberOfMonths = 12,
 ) {
+  if (numberOfMonths === 0) {
+    return 0;
+  }
   const rateResult = rate / 100;
   const futureValueFactor = Math.pow(1 + rateResult, numberOfMonths);
   const numerator = rateResult * futureValueFactor;
