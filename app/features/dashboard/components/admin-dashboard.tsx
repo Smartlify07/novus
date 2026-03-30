3f4346e4894a438a3178fb6cab3cddfbd365fa77
Obinna Anosike <smartlify09@gmail.com>
2026-03-30 22:08:15 +0100

    Feat: Build header section of the admin dashboard

---
+++ app/features/dashboard/components/admin-dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from './admin/header';
+
+export default function AdminDashboard() {
+  return (
+    <div className="flex flex-col gap-10 p-6">
+      <Header />
+    </div>
+  );
+}
