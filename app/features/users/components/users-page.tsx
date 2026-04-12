b3d0ac7bf894cd8c3e113ff7c49da3af2ec121dd
Obinna Anosike <smartlify09@gmail.com>
2026-04-12 13:15:14 +0100

    feat: Build table skeleton

--- app/features/users/components/users-page.tsx
+++ app/features/users/components/users-page.tsx
@@ -3,14 +3,14 @@ import { useState } from 'react';
 import { Header } from './header';
 import { PaginationSection } from './pagination-section';
 import { SummaryCards } from './summary-cards';
-import { UsersList } from './users-list';
+import { UsersTable } from './users-table';
 
 export function UsersPage() {
   return (
     <div className="flex flex-col p-6 gap-10">
       <Header />
       <SummaryCards />
-      <UsersList />
+      <UsersTable />
     </div>
   );
 }
