acbbc283c7d70801800850a906edb24c9382398e
Obinna Anosike <smartlify09@gmail.com>
2026-04-13 02:56:25 +0100

    feat: mobile responsiveness (#65)

--- app/features/loans/components/header.tsx
+++ app/features/loans/components/header.tsx
@@ -1,26 +1,33 @@
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
-import React from 'react';
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
+import { useLoans } from "../hooks";
 
 export default function Header() {
+  const { data: loans } = useLoans();
+  const activeLoans =
+    loans?.loans.filter((loan) => loan.status === "ACTIVE").length || 0;
+  const totalLoans = loans?.loans.length || 0;
   return (
-    <section className="flex items-center justify-between">
+    <section className="flex flex-col justify-between gap-4 lg:flex-row lg:items-center">
       <div className="flex flex-col gap-1">
-        <h3 className="text-xs uppercase">My Loans</h3>
-        <h1 className="text-muted-foreground text-3xl">
-          <span className="text-primary">Loans</span> & Credit
+        <h3 className="text-muted-foreground text-xs uppercase">My Loans</h3>
+        <h1 className="text-foreground text-3xl">
+          <span className="">Loans</span> & Credit
         </h1>
 
-        <p className="text-muted-foreground text-sm flex items-center gap-1">
-          <span>2 active loans</span> <span>.</span> <span>3 total</span>
+        <p className="text-muted-foreground flex items-center gap-1 text-sm">
+          <span>{activeLoans} active loans</span>{" "}
+          <span className="bg-muted-foreground size-0.5 rounded-full"></span>{" "}
+          <span>{totalLoans} total</span>
         </p>
       </div>
 
-      <div className="flex items-center gap-2 self-end">
-        <Button className="" variant={'outline'}>
+      <div className="flex items-center gap-2 lg:self-end">
+        <Button className="" variant={"outline"}>
           Export
         </Button>
-        <Button asChild className="" variant={'default'}>
+        <Button asChild className="" variant={"default"}>
           <Link href="/loans/apply">Apply for a loan</Link>
         </Button>
       </div>
--- app/features/loans/components/summary-cards.tsx
+++ app/features/loans/components/summary-cards.tsx
@@ -1,34 +1,34 @@
-'use client';
-import React from 'react';
-import SummaryCard from '../../dashboard/components/summary-card';
-import { useLoans } from '../hooks';
-import { formatCurrency } from '@/lib/utils';
-import { format } from 'date-fns';
+"use client";
+import React from "react";
+import SummaryCard from "../../dashboard/components/summary-card";
+import { useLoans } from "../hooks";
+import { formatCurrency } from "@/lib/utils";
+import { format } from "date-fns";
 
 export default function SummaryCards() {
   const { data, isPending } = useLoans();
   const loans = data?.loans;
   const combinedMonthlyPayment = loans
-    ?.filter((loan) => loan.status === 'ACTIVE')
+    ?.filter((loan) => loan.status === "ACTIVE")
     .reduce((prev, curr) => prev + curr.monthlyPayment, 0);
   const totalOutstanding = loans?.reduce((prev, curr) => {
     const outstanding = prev + curr.outstandingBalance;
     return outstanding;
   }, 0);
   const totalActiveLoans = loans?.filter(
-    (loan) => loan.status === 'ACTIVE',
+    (loan) => loan.status === "ACTIVE",
   ).length;
   const earliestMaturity = loans
-    ?.filter((loan) => loan.status === 'ACTIVE')
+    ?.filter((loan) => loan.status === "ACTIVE")
     .sort(
       (a, b) =>
         new Date(a.maturityDate).getTime() - new Date(b.maturityDate).getTime(),
     )[0];
   return (
-    <div className="grid grid-cols-3 gap-4">
+    <div className="grid gap-4 lg:grid-cols-3">
       <SummaryCard
         title="Total Outstanding"
-        value={formatCurrency(totalOutstanding ?? 0, 'NGN')}
+        value={formatCurrency(totalOutstanding ?? 0, "NGN")}
         icon={null}
         isLoading={isPending}
       >
@@ -36,7 +36,7 @@ export default function SummaryCards() {
       </SummaryCard>
       <SummaryCard
         title="Combined Monthly"
-        value={formatCurrency(combinedMonthlyPayment ?? 0, 'NGN')}
+        value={formatCurrency(combinedMonthlyPayment ?? 0, "NGN")}
         icon={null}
         isLoading={isPending}
       >
@@ -45,7 +45,7 @@ export default function SummaryCards() {
       <SummaryCard
         title="Earliest maturity"
         value={
-          earliestMaturity ? format(earliestMaturity.maturityDate, 'PPP') : ''
+          earliestMaturity ? format(earliestMaturity.maturityDate, "PPP") : ""
         }
         icon={null}
         isLoading={isPending}
